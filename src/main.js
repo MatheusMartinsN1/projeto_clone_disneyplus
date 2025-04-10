e7147e2c49f9ba35ca13858a72ecd1a3a3f17bb5
MatheusMartinsN1 <mateusdecampos34@icloud.com>
2025-04-10 20:47:41 -0300

    Sections de dispositivos, perguntas e footer

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     let buttons = document.querySelectorAll('[data-tab-button]')
+    let questions = document.querySelectorAll('[data-faq-question]')
     
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao) {
@@ -11,7 +12,18 @@ document.addEventListener('DOMContentLoaded', function() {
             botao.target.classList.add('shows__tabs__button--is-active')
         })
     }
+
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', abreOuFechaResposta)
+    }
 })
+
+function abreOuFechaResposta(elemento) {
+    let classe = 'faq__questions__item--is-open'
+    let elementoPai = elemento.target.parentNode
+
+    elementoPai.classList.toggle(classe)
+}
 
 function removeBotaoAtivo() {
     let buttons = document.querySelectorAll('[data-tab-button]')
